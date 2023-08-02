bf2eed1cde05ba8575ccc8128cba1b0301bba285
obaida28 <obaidaalnaheel283501@gmaul.com>
2023-08-02 09:29:18 +0300

    add mapper folder to API project

---
+++ API/Mapper/CarProfile.cs
@@ -0,0 +1,36 @@
+using API.DTOs;
+using AutoMapper;
+using Core.Entites;
+
+namespace API.Mapper;
+public class CarProfile : Profile
+{
+    public CarProfile()
+    {
+        CreateMap < CarDTO, Car > ()
+        .ForMember(
+            dest => dest.Rentals  ,opt => opt.MapFrom(src => 
+                new List<Rental> { 
+                    new Rental { CustomerId = (Guid)src.CustomerId , 
+                    DriverId = src.DriverId , CarId = src.CarId} }
+            ));
+
+         CreateMap<Car, CarView>()
+            .ForMember(dest => dest.DriverName, opt => opt.MapFrom(src => getDrivers(src)))
+            .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => getCustomers(src)));
+    }
+    private IEnumerable<string> getDrivers(Car list)
+    {
+        if(list.Rentals.Any())
+            return list.Rentals.Where(r => r.Driver.IsAvailable).Select(a => a.Driver.DriverName);
+        IEnumerable<string> res = new[] { "No Driver" };
+        return res;
+    }
+    private IEnumerable<string> getCustomers(Car list)
+    {
+        if(list.Rentals.Any())
+            return list.Rentals.Select(a => a.Customer.CustomerName);
+        IEnumerable<string> res = new[] { "No Customer" };
+        return res;
+    }
+}
