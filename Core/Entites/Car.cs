bc72ae29b86464f9d93579f28a505fea31a6a4fd
obaida28 <obaidaalnaheel283501@gmaul.com>
2023-08-14 11:52:24 +0300

    Make the default value of IsAvailable in Driver or Car is True

--- Core/Entites/Car.cs
+++ Core/Entites/Car.cs
@@ -6,7 +6,7 @@ public class Car : BaseEntity
     public decimal EngineCapacity { get; set; }
     public string Color { get; set; }
     public int DailyRate { get; set; }
-    public bool IsAvailable { get; set; }
+    public bool IsAvailable { get; set; } = true;
     public virtual ICollection<Rental> Rentals { get; set; }
     public Car() : base() {}
 }
--- Core/Entites/Driver.cs
+++ Core/Entites/Driver.cs
@@ -4,7 +4,7 @@ public class Driver : BaseEntity
     public string Name { get; set; }
     public Guid? SubstituteId { get; set; }
     public virtual Driver Substitute { get; set; }
-    public bool IsAvailable { get; set; }
+    public bool IsAvailable { get; set; } = true;
     public virtual ICollection<Rental> Rentals { get; set; }
     public Driver() : base() {}
 }
