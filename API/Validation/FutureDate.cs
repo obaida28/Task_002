4c69fabd1eb4787ebe72ab3a6c3145c0e7a53719
obaida28 <obaidaalnaheel283501@gmaul.com>
2023-08-27 09:30:22 +0300

    fix error for date

--- API/Validation/FutureDate.cs
+++ API/Validation/FutureDate.cs
@@ -2,9 +2,14 @@ namespace API.Validation;
 public class FutureDateAttribute : ValidationAttribute
 {
     public override bool IsValid(object value)
-    {
+    {  
         if (value is DateTime date)
-            return date != DateTime.MinValue && date > DateTime.Now.Date;
+        {
+            var hasValue = date != DateTime.MinValue;
+            if(!hasValue)
+                return true;
+            return date > DateTime.Now.Date;
+        }
         return false;
     }
 }
